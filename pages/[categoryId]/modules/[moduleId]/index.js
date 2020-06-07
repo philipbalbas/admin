ce3356753bc2c64abba0567484d66b14c7a81ded
Philip <philip.balbas@gmail.com>
2020-06-07 22:12:37 +0800

    chore(Module): refactor dynamic file

--- pages/[categoryId]/modules/[moduleId]/index.js
+++ pages/[categoryId]/modules/[moduleId]/index.js
@@ -1,5 +1,5 @@
 // This file is used as a filename shim for the actual
 // App.re Reason component
-import { make } from "../../module.bs";
+import { make } from "../../../module.bs";
 
 export default make;
